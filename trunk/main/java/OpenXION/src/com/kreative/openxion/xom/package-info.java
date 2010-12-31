60b93c031b45f52a3de9907c7daf92eec55fe2dc
beckiergb@gmail.com <beckiergb@gmail.com@ca6a3f96-d605-42f8-d8fc-15a5c3ed5e11>
2010-12-31 23:09:06 +0000

    Copyright dates.

--- trunk/main/java/OpenXION/src/com/kreative/openxion/xom/package-info.java
+++ trunk/main/java/OpenXION/src/com/kreative/openxion/xom/package-info.java
@@ -3,7 +3,7 @@
  * (instances of data types) manipulated by an XION program. Main classes
  * of concern in this package are XOMVariant, XOMVariable, and XOMDataType.
  * <p>
- * Copyright &copy; 2009-2010 Rebecca G. Bettencourt / Kreative Software
+ * Copyright &copy; 2009-2011 Rebecca G. Bettencourt / Kreative Software
  * <p>
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
